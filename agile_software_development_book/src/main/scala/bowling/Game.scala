2071af5c337baa1528e3a6ab693adeec59de46b1
Ryoya ARAI <ryoya_arai@dwango.co.jp>
2020-08-25 00:55:51 +0900

    add spare case

--- agile_software_development_book/src/main/scala/bowling/Game.scala
+++ agile_software_development_book/src/main/scala/bowling/Game.scala
@@ -2,16 +2,43 @@ package bowling
 
 class Game {
   private var itsScore: Int = 0
-  private var itsThrows: Vector[Int] = Vector.empty
+  private var itsThrows: List[Int] = List.empty
+  private var itsCurrentFrame: Int = 1
+  private var firstThrow: Boolean = true
 
   def score: Int = itsScore
 
   def add(pins: Int): Unit = {
     itsThrows = itsThrows :+ pins
     itsScore += pins
+    adjustCurrentFrame
+  }
+
+  def adjustCurrentFrame: Unit = {
+    if (firstThrow) {
+      firstThrow = false
+    } else {
+      firstThrow = true
+      itsCurrentFrame += 1
+    }
   }
 
   def scoreForFrame(frame: Int): Int = {
-    itsThrows.take(frame * 2).sum
+    val frames = itsThrows.grouped(2).take(frame)
+    val nextFrames = itsThrows.grouped(2).take(frame+1).toList.tail :+ List(0, 0)
+    frames.zip(nextFrames).foldLeft(0: Int)((acc: Int, v: (List[Int], List[Int])) => {
+      v._1 match {
+        case firstThrow :: secondThrow :: _ =>
+          if (firstThrow + secondThrow == 10) {
+            acc + firstThrow + secondThrow + v._2.headOption.getOrElse(0)
+          }
+          else {
+            acc + firstThrow + secondThrow
+          }
+        case throws => acc + throws.sum
+      }
+    })
   }
+
+  def getCurrentFrame: Int = itsCurrentFrame
 }
--- agile_software_development_book/src/test/scala/bowling/GameSpec.scala
+++ agile_software_development_book/src/test/scala/bowling/GameSpec.scala
@@ -8,6 +8,10 @@ class GameSpec extends ScalaTestSpec {
   }
 
   "ゲームのスコア計算について" should {
+    "現在のフレームを取得できる" in new SetUp {
+      g.getCurrentFrame shouldEqual 1
+    }
+
     "1度投げた時" in new SetUp {
       g.add(5)
       g.score shouldEqual 5
@@ -17,6 +21,7 @@ class GameSpec extends ScalaTestSpec {
       g.add(5)
       g.add(4)
       g.score shouldEqual 9
+      g.getCurrentFrame shouldEqual 2
     }
 
     "4度投げて一度もマークが取れない時" in new SetUp {
@@ -27,6 +32,25 @@ class GameSpec extends ScalaTestSpec {
       g.score shouldEqual 18
       g.scoreForFrame(1) shouldEqual 9
       g.scoreForFrame(2) shouldEqual 18
+      g.getCurrentFrame shouldEqual 3
+    }
+
+    "スペアを取った時" in new SetUp {
+      g.add(3)
+      g.add(7)
+      g.add(3)
+      g.scoreForFrame(1) shouldEqual 13
+      g.getCurrentFrame shouldEqual 2
+    }
+
+    "スペアを取った後のフレームについて" in new SetUp {
+      g.add(3)
+      g.add(7)
+      g.add(3)
+      g.add(2)
+      g.scoreForFrame(1) shouldEqual(13)
+      g.scoreForFrame(2) shouldEqual(18)
+      g.getCurrentFrame shouldEqual 3
     }
   }
 }
