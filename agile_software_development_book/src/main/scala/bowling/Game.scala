a27c2a0d4a30320831b22bcd990389d6153402da
Ryoya ARAI <ryoya_arai@dwango.co.jp>
2020-08-25 02:39:49 +0900

    add Scorer

--- agile_software_development_book/src/main/scala/bowling/Game.scala
+++ agile_software_development_book/src/main/scala/bowling/Game.scala
@@ -1,24 +1,19 @@
 package bowling
 
 class Game {
-  private var itsScore: Int = 0
-  private var itsThrows: List[Int] = List.empty
   private var itsCurrentFrame: Int = 1
   private var firstThrowInFrame: Boolean = true
 
-  private var ball: Int = 0
-  private var firstThrow: Int = 0
-  private var secondThrow: Int = 0
+  private val itsScorer = new Scorer
 
   def score: Int = scoreForFrame(getCurrentFrame-1)
 
   def add(pins: Int): Unit = {
-    itsThrows = itsThrows :+ pins
-    itsScore += pins
+    itsScorer.addThrow(pins)
     adjustCurrentFrame(pins)
   }
 
-  def adjustCurrentFrame(pins: Int): Unit = {
+  private def adjustCurrentFrame(pins: Int): Unit = {
     if (firstThrowInFrame) {
       if (pins == 10)
         itsCurrentFrame += 1
@@ -31,49 +26,7 @@ class Game {
     itsCurrentFrame = Math.min(11, itsCurrentFrame)
   }
 
-  def scoreForFrame(frame: Int): Int = {
-    ball = 0
-    var score = 0
-    for (currentFrame <- 1 to frame) {
-      firstThrow = itsThrows.apply(ball)
-      if (strike) {
-        score += 10 + nextTwoBallsForStrike
-        ball += 1
-      } else if (spare) {
-        score += 10 + nextBallForSpare
-        ball += 2
-      } else {
-        score += twoBallsInFrame
-        ball += 2
-      }
-    }
-    score
-  }
-
-  private def handleSecondThrow: Int = {
-    var score = 0
-    secondThrow = itsThrows.apply(ball+1)
-
-    val frameScore = firstThrow + secondThrow
-    if (spare) {
-      ball += 2
-      score += frameScore + nextBallForSpare
-    } else {
-      ball += 2
-      score += frameScore
-    }
-    score
-  }
-
-  private def strike: Boolean = itsThrows.apply(ball) == 10
-
-  private def nextTwoBallsForStrike: Int = itsThrows.apply(ball+1) + itsThrows(ball+2)
-
-  private def spare: Boolean = itsThrows.apply(ball) + itsThrows.apply(ball+1) == 10
-
-  private def nextBallForSpare: Int = itsThrows.apply(ball+2)
-
-  private def twoBallsInFrame: Int = itsThrows.apply(ball) + itsThrows.apply(ball+1)
+  def scoreForFrame(frame: Int): Int = itsScorer.scoreForFrame(frame)
 
   def getCurrentFrame: Int = itsCurrentFrame
 }
---
+++ agile_software_development_book/src/main/scala/bowling/Scorer.scala
@@ -0,0 +1,43 @@
+package bowling
+
+class Scorer {
+  private var itsThrows: List[Int] = List.empty
+  private var itsCurrentThrow: Int = 0
+
+  private var ball: Int = 0
+  private var firstThrow: Int = 0
+
+  def addThrow(pins: Int): Unit = {
+    itsThrows = itsThrows :+ pins
+    itsCurrentThrow += 1
+  }
+
+  def scoreForFrame(frame: Int): Int = {
+    ball = 0
+    var score = 0
+    for (currentFrame <- 1 to frame) {
+      firstThrow = itsThrows.apply(ball)
+      if (strike) {
+        score += 10 + nextTwoBallsForStrike
+        ball += 1
+      } else if (spare) {
+        score += 10 + nextBallForSpare
+        ball += 2
+      } else {
+        score += twoBallsInFrame
+        ball += 2
+      }
+    }
+    score
+  }
+
+  private def strike: Boolean = itsThrows.apply(ball) == 10
+
+  private def nextTwoBallsForStrike: Int = itsThrows.apply(ball+1) + itsThrows(ball+2)
+
+  private def spare: Boolean = itsThrows.apply(ball) + itsThrows.apply(ball+1) == 10
+
+  private def nextBallForSpare: Int = itsThrows.apply(ball+2)
+
+  private def twoBallsInFrame: Int = itsThrows.apply(ball) + itsThrows.apply(ball+1)
+}
