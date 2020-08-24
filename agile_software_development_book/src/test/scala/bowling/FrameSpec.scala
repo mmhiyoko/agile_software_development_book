61ce2f06a7aeaab4b0229409cb0320c89de9d7bd
Ryoya ARAI <ryoya_arai@dwango.co.jp>
2020-08-24 22:54:12 +0900

    add build.properties

--- agile_software_development_book/src/test/scala/bowling/FrameSpec.scala
+++ agile_software_development_book/src/test/scala/bowling/FrameSpec.scala
@@ -5,7 +5,7 @@ import helper.ScalaTestSpec
 class FrameSpec extends ScalaTestSpec {
   "スコアの計算について" should {
     "一度も投げていない時" in {
-      val f= new Frame()
+      val f = new Frame()
       f.getScore shouldEqual 0
     }
   }
